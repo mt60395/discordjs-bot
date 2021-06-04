9db17f03c002e46ea03572ebe75498d0dc12483f
mt60395 <63754980+mt60395@users.noreply.github.com>
2021-06-04 10:54:05 -0500

    Fixed image exceptions

--- src/cmds/img/imghandler.js
+++ src/cmds/img/imghandler.js
@@ -53,128 +53,133 @@ module.exports = {
             var baseName = slashSplit[slashSplit.length - 1].split(".")[0] // name of the original file
             return `${baseName}-${crypto.randomBytes(2).toString('hex')}.png`
         }
-        var output = genOutput(Link)
+        var output = genOutput(Link);
 
-        switch(args[0]) {
-            case "rotate":
-                var degree = msg.attachments.size > 0 ? args[1]:args[2]
-                switch (degree) {
-                    case "left":
-                        degree = 90;
-                        break;
-                    case "right":
-                        degree = 270;
-                        break;
-                    default:
-                        degree = Number(degree);
-                        if (!(degree > 0 && degree < 360)) {
-                            return msg.reply(`Invalid degree; must be in the range of [0, 360).${docs}`);
+        const fs = require('fs');
+        (async () => {
+            let input = await jimp.read(Link).catch(()=>{});
+            if (!input) return msg.reply("Invalid image."); // don't manipulate a 0 byte image
+            switch(args[0]) {
+                case "rotate":
+                    var degree = msg.attachments.size > 0 ? args[1]:args[2]
+                    switch (degree) {
+                        case "left":
+                            degree = 90;
+                            break;
+                        case "right":
+                            degree = 270;
+                            break;
+                        default:
+                            degree = Number(degree);
+                            if (!(degree > 0 && degree < 360)) {
+                                return msg.reply(`Invalid degree; must be in the range of [0, 360).${docs}`);
+                            }
+                    }
+                    return require('./rotate').run(msg, Link, degree, output, config.SAVE_IMAGES)
+                    // single argument exists if attachment provided. else, the second argument will be used
+
+                case "resize":
+                    var resolution = "", resolutionY = ""; // resolution is for ones with 'x' and resolutionY is for separated numbers
+                    if (msg.attachments.size > 0) {
+                        resolution = args[1]; // only argument is a resolution
+                        if (args[2]) {
+                            resolutionY = args[2]; // if no x (one argument for the dimensions), set resolutionY
                         }
-                }
-                return require('./rotate').run(msg, Link, degree, output, config.SAVE_IMAGES)
-                // single argument exists if attachment provided. else, the second argument will be used
-
-            case "resize":
-                var resolution = "", resolutionY = ""; // resolution is for ones with 'x' and resolutionY is for separated numbers
-                if (msg.attachments.size > 0) {
-                    resolution = args[1]; // only argument is a resolution
-                    if (args[2]) {
-                        resolutionY = args[2]; // if no x (one argument for the dimensions), set resolutionY
-                    }
-                }
-                else {
-                    resolution = args[2]; // 2 arguments, first is a link so the second is the res argument
-                    if (args[3]) {
-                        resolutionY = args[3];
-                    }
-                }
-
-                resolution = resolution.toLowerCase(); // for the 'x' separating the dimensions
-                resolutionY = resolutionY.toLowerCase();
-                var res = resolution.split("x"); // res[0] is x, res[1] is y. Nothing happens if there is no X and it's a space instead
-                if (resolutionY) {
-                    res = [resolution, resolutionY]; // if there is a space instead
-                }
-
-                // resolution/dimension validity checks
-                if (res[0] != "auto" && res[1] != "auto") {
-                    res[0] = Number(res[0]); res[1] = Number(res[1]);
-                    var bool = res[0] > 1 && res[0] <= 4096; // 1 pixel isn't clickable on Discord and you don't want more than 4k
-                    var bool2 = res[1] > 1 && res[1] <= 4096;
-                    var bool3 = Number.isInteger(res[0]);
-                    var bool4 = Number.isInteger(res[1]); // JIMP errors with non integers
-                    if (!(bool && bool2 && bool3 && bool4)) {
-                        return msg.reply(`Invalid dimension(s). Desired side length must be in the range of [1, 4096).${docs}`);
-                    }
-                }
-                else {
-                    var intDimension; // will be 0 or 1 depending on which dimension is an integer and need to check
-                    if (res[0] != "auto" && res[1] == "auto") { // INTxAUTO
-                        intDimension = 0;
-                    }
-                    else if (res[0] == "auto" && res[1] != "auto") { // AUTOxINT
-                        intDimension = 1;
                     }
                     else {
-                        return msg.reply(`Invalid dimensions. One dimension must be an integer in the range of [1, 4096).${docs}`);
+                        resolution = args[2]; // 2 arguments, first is a link so the second is the res argument
+                        if (args[3]) {
+                            resolutionY = args[3];
+                        }
                     }
 
-                    res[intDimension] = Number(res[intDimension]);
-                    var bool = res[intDimension] > 1 && res[intDimension] <= 4096;
-                    var bool2 = Number.isInteger(res[intDimension]);
-                    if (!(bool && bool2)) {
-                        return msg.reply(`Invalid dimension. One dimension must be an integer in the range of [1, 4096).${docs}`);
+                    resolution = resolution.toLowerCase(); // for the 'x' separating the dimensions
+                    resolutionY = resolutionY.toLowerCase();
+                    var res = resolution.split("x"); // res[0] is x, res[1] is y. Nothing happens if there is no X and it's a space instead
+                    if (resolutionY) {
+                        res = [resolution, resolutionY]; // if there is a space instead
                     }
 
-                    res[intDimension == 0 ? 1:0] = jimp.AUTO; // not int, must be auto
-                }
-                return require('./resize').run(msg, Link, res, output, config.SAVE_IMAGES);
+                    // resolution/dimension validity checks
+                    if (res[0] != "auto" && res[1] != "auto") {
+                        res[0] = Number(res[0]); res[1] = Number(res[1]);
+                        var bool = res[0] > 1 && res[0] <= 4096; // 1 pixel isn't clickable on Discord and you don't want more than 4k
+                        var bool2 = res[1] > 1 && res[1] <= 4096;
+                        var bool3 = Number.isInteger(res[0]);
+                        var bool4 = Number.isInteger(res[1]); // JIMP errors with non integers
+                        if (!(bool && bool2 && bool3 && bool4)) {
+                            return msg.reply(`Invalid dimension(s). Desired side length must be in the range of [1, 4096).${docs}`);
+                        }
+                    }
+                    else {
+                        var intDimension; // will be 0 or 1 depending on which dimension is an integer and need to check
+                        if (res[0] != "auto" && res[1] == "auto") { // INTxAUTO
+                            intDimension = 0;
+                        }
+                        else if (res[0] == "auto" && res[1] != "auto") { // AUTOxINT
+                            intDimension = 1;
+                        }
+                        else {
+                            return msg.reply(`Invalid dimensions. One dimension must be an integer in the range of [1, 4096).${docs}`);
+                        }
 
-            case "mirror":
-                var direction = msg.attachments.size > 0 ? args[1]:args[2]
-                if (typeof direction == 'undefined') {
-                    return msg.reply(`Invalid direction; must be horizontal, vertical, or both.${docs}`)
-                }
-                var validDirection = false, h = false, v = false, both = false // variable both is for the message reply, other vars are for direction/validity checking
-                switch(direction.toLowerCase()) { // checks if the direction is valid
-                    case "h":case "horizontal":
-                        validDirection = true; h = true
-                    break
-                    case "v":case "vertical":
-                        validDirection = true; v = true
-                    break
-                    case "both":case"b":case"vh":case"hv":
-                        validDirection = true; both = true; h = true; v = true
-                }
-                var directions = [h, v]
-                if (!validDirection) return msg.reply(`Invalid direction. It must be horizontal, vertical, or both.${docs}`)
-                return require('./mirror').run(msg, Link, directions, output, config.SAVE_IMAGES)
+                        res[intDimension] = Number(res[intDimension]);
+                        var bool = res[intDimension] > 1 && res[intDimension] <= 4096;
+                        var bool2 = Number.isInteger(res[intDimension]);
+                        if (!(bool && bool2)) {
+                            return msg.reply(`Invalid dimension. One dimension must be an integer in the range of [1, 4096).${docs}`);
+                        }
 
-            case "invert":
-                return require('./invert').run(msg, Link, output, config.SAVE_IMAGES)
+                        res[intDimension == 0 ? 1:0] = jimp.AUTO; // not int, must be auto
+                    }
+                    return require('./resize').run(msg, Link, res, output, config.SAVE_IMAGES);
 
-            case "blur":
-                var r = Number(msg.attachments.size > 0 ? args[1]:args[2])
-                if (isNaN(r) || r <= 0) return msg.reply(`Invalid value provided; must be greater than 0.${docs}`)
-                return require('./blur').run(msg, Link, r, output, config.SAVE_IMAGES)
+                case "mirror":
+                    var direction = msg.attachments.size > 0 ? args[1]:args[2]
+                    if (typeof direction == 'undefined') {
+                        return msg.reply(`Invalid direction; must be horizontal, vertical, or both.${docs}`)
+                    }
+                    var validDirection = false, h = false, v = false, both = false // variable both is for the message reply, other vars are for direction/validity checking
+                    switch(direction.toLowerCase()) { // checks if the direction is valid
+                        case "h":case "horizontal":
+                            validDirection = true; h = true
+                        break
+                        case "v":case "vertical":
+                            validDirection = true; v = true
+                        break
+                        case "both":case"b":case"vh":case"hv":
+                            validDirection = true; both = true; h = true; v = true
+                    }
+                    var directions = [h, v]
+                    if (!validDirection) return msg.reply(`Invalid direction. It must be horizontal, vertical, or both.${docs}`)
+                    return require('./mirror').run(msg, Link, directions, output, config.SAVE_IMAGES)
 
-            case "brightness":
-                var p = Number(msg.attachments.size > 0 ? args[1]:args[2])
-                if (!(p >= -100 && p <= 100)) {
-                    return msg.reply(`Invalid percentage provided; must in the range of [-100, 100].${docs}`) 
-                }
-                return require('./brightness').run(msg, Link, p, output, config.SAVE_IMAGES)
+                case "invert":
+                    return require('./invert').run(msg, Link, output, config.SAVE_IMAGES)
 
-            case "contrast":
-                var p = Number(msg.attachments.size > 0 ? args[1]:args[2])
-                if (!(p >= -100 && p <= 100)) {
-                    return msg.reply(`Invalid percentage provided; must in the range of [-100, 100].${docs}`) 
-                }
-                return require('./contrast').run(msg, Link, p, output, config.SAVE_IMAGES)
+                case "blur":
+                    var r = Number(msg.attachments.size > 0 ? args[1]:args[2])
+                    if (isNaN(r) || r <= 0) return msg.reply(`Invalid value provided; must be greater than 0.${docs}`)
+                    return require('./blur').run(msg, Link, r, output, config.SAVE_IMAGES)
 
-            case "greyscale":
-                require('./greyscale').run(msg, Link, output, config.SAVE_IMAGES)
+                case "brightness":
+                    var p = Number(msg.attachments.size > 0 ? args[1]:args[2])
+                    if (!(p >= -100 && p <= 100)) {
+                        return msg.reply(`Invalid percentage provided; must in the range of [-100, 100].${docs}`) 
+                    }
+                    return require('./brightness').run(msg, Link, p, output, config.SAVE_IMAGES)
 
-        }
+                case "contrast":
+                    var p = Number(msg.attachments.size > 0 ? args[1]:args[2])
+                    if (!(p >= -100 && p <= 100)) {
+                        return msg.reply(`Invalid percentage provided; must in the range of [-100, 100].${docs}`) 
+                    }
+                    return require('./contrast').run(msg, Link, p, output, config.SAVE_IMAGES)
+
+                case "greyscale":
+                    require('./greyscale').run(msg, Link, output, config.SAVE_IMAGES)
+
+            }
+        })();
     }
 }
--- src/cmds/img/uploadhandler.js
+++ src/cmds/img/uploadhandler.js
@@ -16,14 +16,8 @@ module.exports = {
                 else {
                     input.resize(jimp.AUTO, 1920).write(output);
                 }
-                msg.channel.send(success, {files:['./' + output]})
-                .then(()=> {
-                    if (!SAVE_IMAGES) fs.unlink(output, function(){});
-                })
-                .catch(()=>{
-                    console.log("Image upload failed: " + output);
-                    msg.reply("Failed to upload the image. Please retrieve the image manually.");
-                })
+                const uploadhandler = require('./uploadhandler');
+                uploadhandler.handle(input, output, msg, success, SAVE_IMAGES);
             })
         })();
     }
